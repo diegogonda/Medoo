d46ed4150260a4cf7c49e980a01d1256bb9c65d9
Diego <diegogonda@recalvi.es>
2019-10-17 11:26:52 +0200

    Añadir columnas personalizadas

--- medoo/ConnectPriorTrait.php
+++ medoo/ConnectPriorTrait.php
@@ -8,20 +8,24 @@ trait ConnectPriorTrait {
             string $table,
             $join,
             $columns = null,
-            $where = null
+            $where = null,
+            string $idKey = "id",
+            string $parentKey = "parent"
     ) {
         $data = $this->select($table, $join, $columns, $where);
-        return $this->buildTree($data, 0, true);
+        return $this->buildTree($data, 0, $idKey, $parentKey);
     }
 
     public function buildTree(
             array $elements,
-            int $parentId = 0
+            int $parentId = 0,
+            string $idKey = "id",
+            string $parentKey = "parent"
     ): Array {
         $branch = [];
         foreach ($elements as $element) {
-            if ($element['parent'] == $parentId) {
-                $children = $this->buildTree($elements, $element['id']);
+            if ($element[$parentKey] == $parentId) {
+                $children = $this->buildTree($elements, $element[$idKey], $idKey, $parentKey);
                 if ($children) {
                     $element['children'] = $children;
                 }
